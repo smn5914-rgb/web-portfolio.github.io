c4c001c3a942775bbc06dd4b81c72a5c292e5fea
Samiya Naseer <smn5914@psu.edu>
2026-02-15 21:56:11 -0500

    Add bootstrap validation behavior to contact form

--- scripts/main.js
+++ scripts/main.js
@@ -65,3 +65,22 @@ document.addEventListener("DOMContentLoaded", () => {
       form.reset();
     }); 
   } 
+
+  // --------Bootstrap Validation--------
+  (() => {
+    'use strict';
+
+    const forms = document.querySelectorAll(".needs-validation");
+
+    Array.from(forms).forEach((form) => {
+      form.addEventListener('submit', event => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        }, false);
+    });
+  })();
+  
